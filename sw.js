a1b7aca58186f64d52908ba68c0324771af8aaa6
HumeLop <loopez321ab@gmail.com>
2021-11-29 19:21:15 -0600

    [Modify]Notification Push

--- sw.js
+++ sw.js
@@ -1,34 +1,49 @@
-const cacheName = 'appCacheV1';
-const contentToCache = [
-    'index.html',
-    'js/app.js',
-    'manifest.js',
-    'img/icons/',
-    'img/anime-away-face.svg',
-    'img/Anime-icono.jpg',
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-        const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(contentToCache);
-    }));
-});
+workbox.precaching.precacheAndRoute([
+  'index.html',
+  'js/app.js',
+  'img/icons/',
+  'img/anime-away-face.svg',
+  'img/Anime-icono.jpg',
+]);
+workbox.routing.registerRoute(
+  new RegExp('\\.*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'CacheHTML',
+  })
+);
+// const cacheName = 'appCacheV1';
+// const contentToCache = [
+//     'index.html',
+//     'js/app.js',
+//     'manifest.js',
+//     'img/icons/',
+//     'img/anime-away-face.svg',
+//     'img/Anime-icono.jpg',
+// ];
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith((async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) return r;
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-  });
+// self.addEventListener('install', (e) => {
+//     console.log('[Service Worker] Install');
+//     e.waitUntil((async () => {
+//         const cache = await caches.open(cacheName);
+//         console.log('[Service Worker] Caching all: app shell and content');
+//         await cache.addAll(contentToCache);
+//     }));
+// });
+
+// self.addEventListener('fetch', (e) => {
+//     e.respondWith((async () => {
+//       const r = await caches.match(e.request);
+//       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//       if (r) return r;
+//       const response = await fetch(e.request);
+//       const cache = await caches.open(cacheName);
+//       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//       cache.put(e.request, response.clone());
+//       return response;
+//     })());
+//   });
 
 // self.addEventListener('activate', event => {
 
